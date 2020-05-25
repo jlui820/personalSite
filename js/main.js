c26df8da2641ef49f612d5d3580d1c004dcd7e4c
Jeffrey Lui <jlui820@gmail.com>
2020-05-25 16:49:10 -0700

    update background pic

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Jeffrey Lui.", "Software Engineer.", "Developer."],
       typeSpeed: 100,
       loop: true,
     });
